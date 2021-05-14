2f423e24d57e92997696333c8fc58235b3e3e2d1
xjati46 <x_jati_46@yahoo.com>
2021-05-14 21:07:03 +0700

    bug fixed: a tag in a tag

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -11,54 +11,42 @@ function Sidebar() {
     <React.Fragment>
         <Navbar expand="s" variant='dark'>
             <Nav className='center-text'>
-                <Nav.Link href="#">
-                    <NavLink exact to='/'>
-                        <div>
-                            <FontAwesomeIcon icon={faHome} size='2x'/>
-                            Beranda
-                        </div>
-                    </NavLink>
+                <Nav.Link as={NavLink} exact to='/'>
+                    <div>
+                        <FontAwesomeIcon icon={faHome} size='2x'/>
+                        Beranda
+                    </div>
                 </Nav.Link>
-                <Nav.Link>
-                    <NavLink to='/pelatih'>
-                        <div>
-                            <FontAwesomeIcon icon={faUserAstronaut} size='2x'/>
-                            Pelatih
-                        </div>
-                    </NavLink>
+                <Nav.Link as={NavLink} to='/pelatih'>
+                    <div>
+                        <FontAwesomeIcon icon={faUserAstronaut} size='2x'/>
+                        Pelatih
+                    </div>
                 </Nav.Link>
-                <Nav.Link>
-                    <NavLink to='/siswa'>
-                        <div>
-                            <FontAwesomeIcon icon={faUser} size='2x'/>
-                            Siswa
-                        </div>
-                    </NavLink>
+                <Nav.Link as={NavLink} to='/siswa'>
+                    <div>
+                        <FontAwesomeIcon icon={faUser} size='2x'/>
+                        Siswa
+                    </div>
                 </Nav.Link>
-                <Nav.Link>
-                    <NavLink to='/pesanan'>
-                        <div>
-                            <FontAwesomeIcon icon={faFileInvoiceDollar} size='2x'/>
-                            Pesanan
-                        </div>
-                    </NavLink>
+                <Nav.Link as={NavLink} to='/pesanan'>
+                    <div>
+                        <FontAwesomeIcon icon={faFileInvoiceDollar} size='2x'/>
+                        Pesanan
+                    </div>
                 </Nav.Link>
-                <Nav.Link>
-                    <NavLink to='/verifikasi'>
-                        <div>
-                            <FontAwesomeIcon icon={faTasks} size='2x'/>
-                            Verifikasi
-                        </div>
-                    </NavLink>
+                <Nav.Link as={NavLink} to='/verifikasi'>
+                    <div>
+                        <FontAwesomeIcon icon={faTasks} size='2x'/>
+                        Verifikasi
+                    </div>
                 </Nav.Link>
                 <div className='divider'></div>
-                <Nav.Link>
-                    <NavLink to='/logout'>
-                        <div>
-                            <FontAwesomeIcon icon={faSignOutAlt} size='2x'/>
-                            Keluar
-                        </div>
-                    </NavLink>
+                <Nav.Link as={NavLink} to='/logout'>
+                    <div>
+                        <FontAwesomeIcon icon={faSignOutAlt} size='2x'/>
+                        Keluar
+                    </div>
                 </Nav.Link>
             </Nav>
         </Navbar>
